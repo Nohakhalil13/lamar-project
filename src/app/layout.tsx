6ffbd0d467cb9b3912f091cd675192535781bf1b
noha khalil <nohaaoo13@gmail.com>
2026-09-02 17:34:57 +0300

    OGg photo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
     locale: 'nl_NL',
     url: SITE_URL,
     siteName: SITE_NAME,
-    images: [{ url: absoluteUrl('/og-image.png'), width: 1200, height: 630, alt: SITE_NAME }],
+    images: [{ url: absoluteUrl('/ogg-image.png'), width: 1200, height: 630, alt: SITE_NAME }],
   },
   twitter: {
     card: 'summary_large_image',
